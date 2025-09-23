4c5bdb28ad3d6e6eeb7a64e3c2942e9216ab13a1
LinearCone74 <130677881+LinearCone74@users.noreply.github.com>
2025-09-23 08:50:55 +0700

    Update kiem tra 3 canh tam giac.py

--- kiem tra 3 canh tam giac.py
+++ kiem tra 3 canh tam giac.py
@@ -1,6 +1,13 @@
+import math
+
 a, b, c = map(float, input("Nhập 3 cạnh tam giac:").split())
 
 if ((a+b > c) and (b+c > a) and (c+a > b)):
     print("Đây là 3 cạnh tam giác")
+    p = (a + b + c) / 2
+    print("Chu vi là:", 2 * p)
+    s = p * (p-a) * (p-b) * (p-c)
+    print("Diện tích là:", math.sqrt(s))
 else:
     print("Không phải 3 cạnh tam giác")
+
