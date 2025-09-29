84a7430fd55d24fa0202319071a4618798aa6a13
LinearCone74 <130677881+LinearCone74@users.noreply.github.com>
2025-09-29 21:05:58 +0700

    Update test.py

--- test.py
+++ test.py
@@ -6,10 +6,9 @@ def check (i):
 
 def ans (i):
     k = ord(i)
-    if ((k >= 65) and (k < 90)): return chr(k + 33)
-    elif ((k >= 97) and (k < 122)): return chr(k - 31)
-    elif (k == 90): return chr(97)
-    return chr(97)
+    if ((k >= 65) and (k <= 90)): return chr(k + 32)
+    else: return chr(k - 32)
+
 
 i = input("Nhập kí tự: ")
 
@@ -17,4 +16,5 @@ while (check(i) == False):
     i = input("Vui long nhap lai: ")
     
 print(ans(i))
+
 
